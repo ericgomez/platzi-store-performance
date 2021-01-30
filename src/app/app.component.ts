1a558229f31d9eac035ec0130b8ad800fdf5ba13
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-30 10:14:13 -0600

    Uso de ngFor add y delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store-complete';
+
+  items = ['eric', 'natalia', 'ines']
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
