380ff805ca3ef53280387799123f541854ffd20b
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-02 13:00:37 -0600

    Haciendo una solicitud GET desde el servicio

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -21,7 +21,15 @@ product!: Product;
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
-      //this.product = this.productsService.getProduct(id)!;
+      this.fetchProduct(id);
+      // this.product = this.productsService.getProduct(id)!;
+    })
+  }
+
+  fetchProduct(id: string) {
+    this.productsService.getProduct(id)
+    .subscribe(product => {
+      this.product = product;
     })
   }
 
