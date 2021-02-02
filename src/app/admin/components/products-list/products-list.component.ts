46522a43969b6748c1ce044b5c0eeb1cbda08409
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-02 16:23:06 -0600

    Lista de inventario y detalle

---
+++ src/app/admin/components/products-list/products-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ProductsService } from './../../../core/services/products/products.service'
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.scss']
+})
+export class ProductsListComponent implements OnInit {
+
+  products = [] as  any;
+  displayedColumns: string[] = ['id', 'title', 'price', 'actions'];
+
+  constructor(private productsService: ProductsService) { }
+
+  ngOnInit(): void {
+  }
+
+  fetchProducts() {
+    this.productsService.getAllProducts()
+      .subscribe(products => {
+        this.products = products;
+      })
+  }
+}
