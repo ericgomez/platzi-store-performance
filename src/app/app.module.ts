ec5b393188063465b60edca2afadc4eaf9c82cbe
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-02 09:30:23 -0600

    Instalando Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component'
 
 import { SharedModule } from './shared/shared.module'
-import { CoreModule } from './core/core.module'
+import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { CoreModule } from './core/core.module'
     AppRoutingModule,
     FormsModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
