a8af52fd5fa0abcce649424b5d77f8e498f4347b
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-05 12:23:18 -0600

    Implementando Auth y Guards

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -16,4 +16,19 @@ export class AuthService {
   ) {
     return this.af.createUserWithEmailAndPassword(email, password);
   }
+
+  login (
+    email: string,
+    password: string
+  ) {
+    return this.af.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.af.signOut();
+  }
+
+  hasUser() {
+    return this.af.authState;
+  }
 }
