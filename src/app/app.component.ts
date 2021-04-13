cd5d8597b8e3233f6abd19f8b01becdc405fbe55
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-13 16:05:30 -0500

    Google Analytics y Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+import { filter } from 'rxjs/operators'
+
+declare var gtag: any;
+
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private router: Router
+  ) {
+    const navEndEvents$ = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      );
+
+      navEndEvents$.subscribe((event: any) => {
+        gtag('config', 'G-KV28M9Y0Q8', {
+          page_title: event.urlAfterRedirects,
+          page_path: event.urlAfterRedirects
+        });
+      });
+  }
 }
