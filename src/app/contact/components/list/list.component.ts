14503b1c92f370ef6e82d35b994b6d3b7fcc5f1c
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-14 11:17:30 -0500

    Optimización de componentes con OnPush en Change Detection

--- src/app/contact/components/list/list.component.ts
+++ src/app/contact/components/list/list.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ChangeDetectionStrategy } from '@angular/core';
 
 import { EmployeeData } from './../../../core/models/employeeData.model';
+
+// Funcion para calcular el fibonacci de un numero
+const fibonacci = (num: number): number => {
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush // OnPush: Detecta a que componente enverdad esta realizinado el cambio y solo hace la deteccion de cambios a ese componente como tal
 })
 export class ListComponent implements OnInit {
 
@@ -27,6 +36,11 @@ export class ListComponent implements OnInit {
 
     // Limpiamos el label
     this.label = '';
+  }
+
+  calc(item: EmployeeData) {
+    console.log('list', this.title); 
+    return fibonacci(item.num);
   }
 
 }
